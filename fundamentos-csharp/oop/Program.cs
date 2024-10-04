4ec17bfbcc2a65a880af0c0643131fd12d03769c
Donato Monzon <spartanhalokitty@gmail.com>
2024-10-04 08:52:44 -0600

    fix: :fire: remove nested projects

--- fundamentos-csharp/oop/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-namespace OOP {
-    class Bebida {
-        public string Nombre { get; set; }
-    }
-}
